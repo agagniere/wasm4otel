583a3247d147c2636222974dc0e08f1b51b10bf7
Antoine GAGNIERE <antoine.gagniere@datadoghq.com>
2025-12-19 22:38:02 +0100

    Organize zig source files in folders

---
+++ zig/freestanding/helloworld.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const build_info = @import("build_info");
+
+pub const std_options: std.Options = .{
+    .logFn = @import("hostlog").logFn,
+    .log_level = .debug,
+};
+
+comptime {
+    @export(&init, .{ .name = "_start" });
+}
+
+/// Called when loaded
+fn init() callconv(.{ .wasm_mvp = .{} }) void {
+    std.log.info("Hello from {t} {t} {t}", .{
+        builtin.cpu.arch,
+        builtin.os.tag,
+        builtin.abi,
+    });
+}
+
+export fn start() void {
+    std.log.info("Starting {s}, part of {s} version {s}, built with Zig {s}", .{
+        @src().file,
+        build_info.name,
+        build_info.version,
+        builtin.zig_version_string,
+    });
+}
+
+export fn stop() void {
+    std.log.info("So long !", .{});
+}
--- zig/src/log.zig
+++ zig/src/log.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+/// Used to issue a log to be forwared to zap
+extern fn host_log(level: i32, ptr: [*]const u8, size: u32) void;
+
 /// https://pkg.go.dev/go.uber.org/zap@v1.27.1/zapcore#Level
 const LogLevel = enum(i8) {
     debug = -1,
@@ -20,13 +23,21 @@ const LogLevel = enum(i8) {
     }
 };
 
-/// Used to issue a log to be forwared to zap
-extern fn host_log(level: i32, ptr: [*]const u8, size: u32) void;
-
+/// Forward this string to zap
 pub fn hostLog(level: LogLevel, string: []const u8) void {
     host_log(@intFromEnum(level), string.ptr, string.len);
 }
 
+/// Format then forward this log to zap
+pub fn hostLogFormat(level: LogLevel, format: []const u8, args: anytype) void {
+    var buffer: [256]u8 = undefined;
+    var writer: std.Io.Writer = .fixed(&buffer);
+    writer.print(format, args) catch unreachable;
+    hostLog(level, writer.buffered());
+}
+
+/// To be used as callback of the standard log,
+/// to forward std.log calls to zap
 pub fn logFn(
     comptime level: std.log.Level,
     comptime scope: @EnumLiteral(),
